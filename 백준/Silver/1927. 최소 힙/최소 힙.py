826175eea71faacbeec2252cbaa49f46427ef40e
Moon <116760540+barista00@users.noreply.github.com>
2023-01-28 22:41:57 +0900

    [Silver II] Title: 최소 힙, Time: 232 ms, Memory: 118264 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,13 @@
+import heapq, sys
+lst = []
+heapq.heapify(lst)
+N = int(sys.stdin.readline())
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(lst, num)
+    else:
+        if lst:
+            print(heapq.heappop(lst))
+        else:
+            print(0)

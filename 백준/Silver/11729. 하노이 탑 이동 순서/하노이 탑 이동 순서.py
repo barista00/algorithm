63aefe3305e622bcfcfd73ef267e26b3867fbf1b
Moon <116760540+barista00@users.noreply.github.com>
2023-03-01 03:56:35 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 524 ms, Memory: 153628 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,11 @@
+def hanoi(n, start, end, goal):
+    if n == 1:
+        print(start, end)
+    else:
+        hanoi(n-1, start, goal, end)
+        print(start, end)
+        hanoi(n-1, goal, end, start)
+
+N = int(input())
+print(2**N - 1)
+hanoi(N, 1, 3, 2)

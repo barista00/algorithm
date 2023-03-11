92b77c6508fb8269ea33b1c863b03c458046e1fc
Moon <116760540+barista00@users.noreply.github.com>
2023-03-11 23:25:42 +0900

    [Bronze III] Title: ACM 호텔, Time: 144 ms, Memory: 115636 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -2,15 +2,15 @@ T = int(input())
 for _ in range(T):
     h, w, n = map(int, input().split())
 
-    호수 = 1
+    th = 1
     while True:
         if n - h <= 0:
             break
         else:
             n -= h
-            호수 += 1
+            th += 1
     
-    if 호수 >= 10:
-        print(f'{n}{호수}')
+    if th >= 10: # 호수가 10넘어가면 1층 10호면 110호가 나와야하기 때문에 2가지 출력결과를 대비해야됨
+        print(f'{n}{th}')
     else:
-        print(f'{n}0{호수}')
+        print(f'{n}0{th}') # 호수가 10이 넘지않으면 중간에 0을 넣어줘야함

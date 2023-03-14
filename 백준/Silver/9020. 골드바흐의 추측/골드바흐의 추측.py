69accd71b9e5d373b7a5b3168e9090665ed0d48a
Moon <116760540+barista00@users.noreply.github.com>
2023-03-14 20:58:14 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 5376 ms, Memory: 118372 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -3,11 +3,11 @@ import math
 T = int(input())
 for _ in range(T):
     n = int(input())
-    prime = [True for _ in range(n+1)]
-    prime[0] = False
-    prime[1] = False
-
-    for i in range(2, int(math.sqrt(n))+1):
+    prime = [True for _ in range(n+1)] # 에라토스테네스의 체 
+    prime[0] = False                    # 2부터 특정 수 n까지 총 소수가 몇개인지??구한다
+    prime[1] = False                    # True가 n+1개 들어간 리스트생성 
+                                        # 2부터 sqrt(n)+1까지 소수인 수를 찾고 소수면 그 수의 배수를 전부 False처리한다(배수라는 얘기는 약수임)
+    for i in range(2, int(math.sqrt(n))+1): # 리스트에 남은 True의 ?번째 수들은 모두 소수이다 2번째 수가 True면 2가 소수라는 얘기
         if prime[i] == True:
             multi = 2
             while n >= multi * i:
